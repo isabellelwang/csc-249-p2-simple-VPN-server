da6aa82d9754b38d864388a6252787ffc403c09f
Isabelle Wang <ilwang@smith.edu>
2024-10-08 21:16:22 -0400

    Finished coding and adding comments

--- VPN.py
+++ VPN.py
@@ -3,6 +3,8 @@
 import socket
 import arguments
 import argparse
+from arguments import _ip_address
+from arguments import _port
 
 # Run 'python3 VPN.py --help' to see what these lines do
 parser = argparse.ArgumentParser('Send a message to a server at the given address and prints the response')
@@ -14,14 +16,18 @@ VPN_IP = args.VPN_IP  # Address to listen on
 VPN_PORT = args.VPN_port  # Port to listen on (non-privileged ports are > 1023)
 
 def parse_message(message):
-    message = message.decode("utf-8")
+    '''
+    Takes the message from the client and parses the Server IP, Server port and message into variables
+    '''
+    message = message.decode("utf-8") #decode the message
+
     # Parse the application-layer header into the destination SERVER_IP, destination SERVER_PORT,
     # and message to forward to that destination
+    split_data = message.split(' ') #split the String data by ' ' into a list
+    SERVER_IP = split_data[2] #server IP is the third element in the list
 
-    split_data = message.split(" ")
-    SERVER_IP = message[2]
-    SERVER_PORT = message[3]
-    message = message[4]
+    SERVER_PORT = int(split_data[3]) #server port is the fourth element in the list, converts it to an int
+    message = ' '.join(split_data[4:]) #Rejoin the remaining elements from the list into a string
 
     return SERVER_IP, SERVER_PORT, message
 
@@ -38,13 +44,39 @@ def parse_message(message):
 # client appropriate error messages.
 
 print("VPN starting - connecting to client at IP", VPN_IP, " and port ", VPN_PORT)
-with socket.socket(socket.AF_INET, socket.SOCKET_STREAM) as s: 
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s: 
     #binds to VPN IP and VPN Port
     s.bind((VPN_IP, VPN_PORT))
     #listens for the client
     s.listen()
     #accepts connection with client
     conn, addr = s.accept()
+
+    with conn: 
+        print(f"Connected established with {addr}")
+        while True: 
+            client_data = conn.recv(1024)
+            print(f"Received response from client: '{client_data}' [{len(client_data)} bytes]")
+
+            if not client_data:
+                break 
+            
+            print("Parsing the message from the client...")
+            SERVER_IP, SERVER_PORT, MSG = parse_message(client_data)
+    
+            print("creating a new socket and connecting to server at IP", SERVER_IP, "and port", SERVER_PORT)
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as o:
+                o.connect((SERVER_IP, SERVER_PORT)) #connect the socket to the server ip and port
+                print(f"connection established, sending message to server ' {MSG}")
+                o.sendall(bytes(MSG, 'utf-8')) #encode the parsed message and send to the server
+                print("message sent to server, waiting for echo response")
+                server_data = o.recv(1024).decode('utf-8') #receive the data from the server and decode the data
+                print(f"Received response from server: '{server_data}' [{len(server_data)} bytes]")
+            
+            print("sending to client '{server_data}'")
+            conn.sendall(bytes(server_data, 'utf-8')) #Sending the data from the server to the client using the outer socket
+    
+    print("VPN is done!")
     
 
 
--- client.py
+++ client.py
@@ -20,21 +20,28 @@ VPN_PORT = args.VPN_port  # The port used by the server
 MSG = ' '.join(args.message) # The message to send to the server
 
 def encode_message(message):
+    ''' 
+    Encode the message by adding a header with information about VPN IP/PORT and SERVER IP/PORT
+    Return the message with header to the message.
+    '''
     # Add an application-layer header to the message that the VPN can use to forward it
-    app_header = str(VPN_IP) + " " + str(VPN_PORT) + " " + str(SERVER_IP) + " " + str(SERVER_PORT) 
-    message = app_header + " " + message; 
+    #header includes VPN_IP, VPN_Port, SERVER_IP, SERVER_PORT
+    app_header = str(VPN_IP) + " " + str(VPN_PORT) + " " + str(SERVER_IP) + " " + str(SERVER_PORT)
+
+    #attach the header to the message
+    message = app_header + " " + message
 
     return message
 
 
 print("client starting - connecting to VPN at IP", VPN_IP, "and port", VPN_PORT)
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((VPN_IP, VPN_PORT))
-    print(f"connection established, sending message '{encode_message(MSG)}'")
-    print(encode_message(MSG))
-    s.sendall(bytes(MSG, 'utf-8'))
+    s.connect((VPN_IP, VPN_PORT)) #connecting to the VPN IP and Port
+    MSG = encode_message(MSG) #encoding the message by adding a header with VPN/Server Ip and Port
+    print(f"connection established, sending message '{MSG}'")
+    s.sendall(bytes(MSG, 'utf-8')) #sending the message to the VPN
     print("message sent, waiting for reply")
-    data = s.recv(1024).decode("utf-8")
+    data = s.recv(1024).decode("utf-8") #receiving the message from the server and decoding it
 
-print(f"Received response: '{data}' [{len(data)} bytes]")
+print(f"Received response: '{data}' [{len(data)} bytes]") #received response from the server.
 print("client is done!")
